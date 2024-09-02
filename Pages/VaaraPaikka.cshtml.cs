2ff33e210039a994484b98f467ece8087efd5019
Eltsu22222 <56360185+Eltsu22222@users.noreply.github.com>
2024-09-02 10:03:59 +0300

    Väärä paikka

---
+++ Pages/VaaraPaikka.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mysteeriprojekti.Pages
+{
+    public class VaaraPaikkaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
